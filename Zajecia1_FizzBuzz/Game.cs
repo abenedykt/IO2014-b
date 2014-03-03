eac09d5537d8fc429a9ba72ad876c663d7244486
Wojciech Wróbel <wrobel_w@wp.pl>
2014-03-03 12:33:24 +0100

    Zajecia1

---
+++ Zajecia1_FizzBuzz/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zajecia1_FizzBuzz
+{
+    class Game
+    {
+        internal object Say(int p)
+        {
+            switch (p)
+            {
+                case int.MaxValue:
+                    return "MaxInt";
+                case int.MinValue:
+                    return "MinInt";
+                case 15:
+                    return "FizzBazz";
+                case 5:
+                    return "Bazz";
+                case 3:
+                    return "Fizz";
+                default:
+                    return p;
+            }
+            //if (p == int.MaxValue)
+            //    return "MaxInt";
+            //if (p == int.MinValue)
+            //    return "MinInt";
+            //if (p % 15 == 0)
+            //    return "FizzBazz";
+            //if (p % 5 == 0)
+            //    return "Bazz";
+            //if (p % 3 == 0)
+            //    return "Fizz";
+            //return p;
+        }
+    }
+}
---
+++ Zajecia1_FizzBuzz/Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Zajecia1_FizzBuzz
+{
+    public class Test
+    {
+        [Fact]
+        public void Dla_Wartosci_3_Zwroc_Fizz()
+        {
+            var game = new Game();
+            var r = game.Say(3);
+            Assert.Equal("Fizz", r);
+        }
+
+        [Fact]
+        public void Dla_Wartosci_5_Zwroc_Bazz()
+        {
+            var game = new Game();
+            var r = game.Say(5);
+            Assert.Equal("Bazz", r);
+        }
+
+        [Fact]
+        public void Dla_Wartosci_15_Zwroc_FizzBazz()
+        {
+            var game = new Game();
+            var r = game.Say(15);
+            Assert.Equal("FizzBazz", r);
+        }
+
+        [Fact]
+        public void Dla_Wartosci_MaxInt_Zwroc_MaxInt()
+        {
+            var game = new Game();
+            var r = game.Say(int.MaxValue);
+            Assert.Equal("MaxInt", r);
+        }
+
+        [Fact]
+        public void Dla_Wartosci_MinInt_Zwroc_MaxInt()
+        {
+            var game = new Game();
+            var r = game.Say(int.MinValue);
+            Assert.Equal("MinInt", r);
+        }
+    }
+}
