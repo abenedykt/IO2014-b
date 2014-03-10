35f6efa3cbf430a8dd892ef9cfa7526907c2ddbe
RemigiuszBogdal <doger0@gmail.com>
2014-03-10 18:08:49 +0100

    obecność

---
+++ IO/GeoParserTest/GeoImport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoParserTest
+{
+    class GeoImport
+    {
+        private IGeoRepository repository;
+        private IGeoFileReader fileReader;
+
+        public GeoImport(IGeoFileReader fileReader, IGeoRepository repository)
+        {
+            this.fileReader = fileReader;
+            this.repository = repository;
+        }
+
+        public Dictionary<string, object> Parse()
+        {
+            var tempList = new Dictionary<string, object>();
+            
+            var list = fileReader.GetRecords();
+            foreach (var item in list)
+            {
+                
+            }
+            return tempList;
+        }
+    }
+}
---
+++ IO/GeoParserTest/IGeoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoParserTest
+{
+    public interface IGeoRepository
+    {
+        void Add(string numerDzialki, double x, double y, double minX, double minY, double maxX, double maxY);
+    }
+}
