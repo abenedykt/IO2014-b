85ca0bdabfb98533fca9a28579f0124800f6ab2f
AndrewKrawiec <andrewkrawiec@wp.pl>
2014-03-10 18:16:18 +0100

    2Zadanie

---
+++ AndrzejKrawiec/Geo/GeoImport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geo
+{
+    class GeoImport
+    {
+        public GeoImport(IGeoFileReader file, IGeoRepository repository)
+        {
+
+        }
+    }
+}
