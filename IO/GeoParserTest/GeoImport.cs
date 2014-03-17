e3b49f45b088147ec6d3a5bfb289fd683bed5da7
RemigiuszBogdal <doger0@gmail.com>
2014-03-17 18:00:26 +0100

    no message

--- IO/GeoParserTest/GeoImport.cs
+++ IO/GeoParserTest/GeoImport.cs
@@ -23,7 +23,24 @@ namespace GeoParserTest
             var list = fileReader.GetRecords();
             foreach (var item in list)
             {
-                
+                var linia = item.Split(new string[] {"\n"}, StringSplitOptions.RemoveEmptyEntries);
+                tempList.Add("nrDzialki", linia[0].Trim());
+                var xy = linia[1].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                tempList.Add("x", xy[0].Trim());
+                tempList.Add("y", xy[1].Trim());
+                double minx = Double.MaxValue, miny = Double.MaxValue, maxx = 0, maxy = 0;
+                for (int i = 3; i < int.Parse(linia[2].Trim())+3; i++)
+                {
+                    var minmax = linia[i].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    minx = minx > Convert.ToDouble(minmax[1].Trim()) ? Convert.ToDouble(minmax[1].Trim()) : minx;
+                    miny = miny > Convert.ToDouble(minmax[2].Trim()) ? Convert.ToDouble(minmax[2].Trim()) : miny;
+                    maxx = maxx < Convert.ToDouble(minmax[3].Trim()) ? Convert.ToDouble(minmax[3].Trim()) : maxx;
+                    maxy = maxy < Convert.ToDouble(minmax[4].Trim()) ? Convert.ToDouble(minmax[4].Trim()) : maxy;
+                }
+                tempList.Add("minx", minx);
+                tempList.Add("miny", miny);
+                tempList.Add("maxx", maxx);
+                tempList.Add("maxy", maxy);
             }
             return tempList;
         }
--- IO/GeoParserTest/TestParser.cs
+++ IO/GeoParserTest/TestParser.cs
@@ -34,12 +34,12 @@ namespace GeoParserTest
             var returning = import.Parse();
 
             Assert.Equal("1.1-800/128", returning["nrDzialki"]);
-            Assert.Equal(5544943.71, returning["x"]);
-            Assert.Equal(6532099.62, returning["y"]);
-            Assert.Equal(5544907.94, returning["minx"]);
-            Assert.Equal(6532075.96, returning["miny"]);
-            Assert.Equal(5544975.70, returning["maxx"]);
-            Assert.Equal(6532117.26, returning["maxy"]);
+            Assert.Equal("5544943.71", returning["x"]);
+            Assert.Equal("6532099.62", returning["y"]);
+            Assert.Equal("5544907.94", returning["minx"]);
+            Assert.Equal("6532075.96", returning["miny"]);
+            Assert.Equal("5544975.70", returning["maxx"]);
+            Assert.Equal("6532117.26", returning["maxy"]);
 
         }
     }
