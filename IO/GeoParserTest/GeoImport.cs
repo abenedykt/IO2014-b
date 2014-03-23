1ad488204b10f7d4a0570917850b9721c70ced7f
RemigiuszBogdal <doger0@gmail.com>
2014-03-23 23:10:58 +0100

    Parser działa

--- IO/GeoParserTest/GeoImport.cs
+++ IO/GeoParserTest/GeoImport.cs
@@ -26,16 +26,20 @@ namespace GeoParserTest
                 var linia = item.Split(new string[] {"\n"}, StringSplitOptions.RemoveEmptyEntries);
                 tempList.Add("nrDzialki", linia[0].Trim());
                 var xy = linia[1].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-                tempList.Add("x", xy[0].Trim());
-                tempList.Add("y", xy[1].Trim());
+                tempList.Add("x", double.Parse(xy[0].Replace('.',',')));
+                tempList.Add("y", double.Parse(xy[1].Replace('.', ',')));
                 double minx = Double.MaxValue, miny = Double.MaxValue, maxx = 0, maxy = 0;
                 for (int i = 3; i < int.Parse(linia[2].Trim())+3; i++)
                 {
                     var minmax = linia[i].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-                    minx = minx > Convert.ToDouble(minmax[1].Trim()) ? Convert.ToDouble(minmax[1].Trim()) : minx;
-                    miny = miny > Convert.ToDouble(minmax[2].Trim()) ? Convert.ToDouble(minmax[2].Trim()) : miny;
-                    maxx = maxx < Convert.ToDouble(minmax[3].Trim()) ? Convert.ToDouble(minmax[3].Trim()) : maxx;
-                    maxy = maxy < Convert.ToDouble(minmax[4].Trim()) ? Convert.ToDouble(minmax[4].Trim()) : maxy;
+                    double currMinX = double.Parse(minmax[1].Replace('.',','));
+                    double currMinY = double.Parse(minmax[2].Replace('.', ','));
+                    double currMaxX = double.Parse(minmax[3].Replace('.', ','));
+                    double currMaxY = double.Parse(minmax[4].Replace('.', ','));
+                    minx = minx > currMinX ? currMinX : minx;
+                    miny = miny > currMinY ? currMinY : miny;
+                    maxx = maxx < currMaxX ? currMaxX : maxx;
+                    maxy = maxy < currMaxY ? currMaxY : maxy;
                 }
                 tempList.Add("minx", minx);
                 tempList.Add("miny", miny);
