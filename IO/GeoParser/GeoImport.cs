edab9a61fbe543926ad3a1207fd05cf6397ed303
GrzegorzJanik <grzegorz0011@gmail.com>
2014-03-23 11:57:39 +0100

    Update Parsera.

--- IO/GeoParser/GeoImport.cs
+++ IO/GeoParser/GeoImport.cs
@@ -15,9 +15,24 @@ namespace GeoParser
             this.fileReader = fileReader;
             this.repository = repository;
         }
-        internal object Parse()
+        public List<string> Parse()
         {
-            throw new NotImplementedException();
+            var parsed = new List<string>();
+            var record = fileReader.GetRecords();
+            var array = record.FirstOrDefault().Split(new[] { '\r', '\n' });
+            var xs = new List<double>();
+            var ys = new List<double>();
+            parsed.Add(array[0]);
+            parsed.Add(array[2].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0]);
+            parsed.Add(array[2].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[1]);
+            for (int i = 6; i < 18; i = i + 2)
+            {
+                xs.Add(double.Parse(array[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[1]));
+                ys.Add(double.Parse(array[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[2]));
+            }
+            parsed.Add(xs.Max().ToString());
+            parsed.Add(ys.Max().ToString());
+            return parsed;
         }
     }
 }
--- IO/GeoParser/Tests.cs
+++ IO/GeoParser/Tests.cs
@@ -18,6 +18,11 @@ namespace GeoParser
 
             var returns = import.Parse();
 
+            Assert.Equal("1.1-800/128", returns[0]); //nrDzialki
+            Assert.Equal("5544943.71", returns[1]); //x
+            Assert.Equal("6532099.62", returns[2]); //y
+            Assert.Equal("5544975.7", returns[3]);
+            Assert.Equal("6532117.26", returns[4]);
         }
     }
 }
