9a3d1c6e7d629abd96305518160ec7e45210aec7
LucasBartoszewski <khoga91@gmail.com>
2014-03-24 12:26:09 +0100

    Dodanie DefaultTown.cs ---> Parse(), która wyciąga X, Y, MinX, MinY, MaxX, MaxY ze Stringu oraz odpowiedniego testu.

--- LukaszBartoszewski/Zad2 - Geo/GeoInport.cs
+++ LukaszBartoszewski/Zad2 - Geo/GeoInport.cs
@@ -5,16 +5,44 @@ using System.Text;
 
 namespace Zad2
 {
-    public class GeoInport
+    public class GeoImport
     {
         private IGeoFileReader fileReader;
         private IGeoRepository repository;
 
-        public GeoInport(IGeoFileReader fileReader, IGeoRepository repository)
+        public GeoImport(IGeoFileReader fileReader, IGeoRepository repository)
         {
             // TODO: Complete member initialization
             this.fileReader = fileReader;
             this.repository = repository;
+        }
+
+        public DefaultTown Parse()
+        {
+            var defaultTown = new DefaultTown();
+
+            var tabRecords = defaultTown.defaultTown.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var getXandY = defaultTown.defaultTown.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
+
+            var x = new List<double>();
+            var y = new List<double>();
+
+            for (var i = 3; i < tabRecords.Length; i++)
+            {
+                var tempXy = tabRecords[i].Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
+                x.Add(Convert.ToDouble(tempXy[1]));
+                y.Add(Convert.ToDouble(tempXy[2]));
+            }
+
+            defaultTown.GeoNumber = tabRecords[0];
+            defaultTown.x = Convert.ToDouble(getXandY[1]);
+            defaultTown.y = Convert.ToDouble(getXandY[2]); 
+            defaultTown.maxX = x.Max();
+            defaultTown.maxY = y.Max();
+            defaultTown.minX = x.Min();
+            defaultTown.minY = y.Min();
+            return defaultTown;
         }
     }
 }
--- LukaszBartoszewski/Zad2 - Geo/Tests.cs
+++ LukaszBartoszewski/Zad2 - Geo/Tests.cs
@@ -11,110 +11,38 @@ namespace Zad2
     public class GeoTests
     {
         [Fact]
-        public void checkIfFileReaderReturnsString()
-        {
-            var fileReader = Substitute.For<IGeoFileReader>();
-
-            string s = @"1.1-800/128
-        5544943.71        6532099.62         5544943.71        6532099.62 N  7
-7
-              1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7
-              1-7880        5544908.34        6532098.97        5544908.34        6532098.97   0 N N  7
-              1-7807        5544907.94        6532075.96        5544907.94        6532075.96   0 N N  7
-              1-7856        5544928.59        6532085.45        5544928.59        6532085.45   0 N N  7
-              1-7881        5544975.70        6532098.40        5544975.70        6532098.40   0 N N  7
-              1-7907        5544972.66        6532107.54        5544972.66        6532107.54   0 N N  7
-              1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7";
-
-            fileReader.GetRecords().Returns(new List<string> { s }); //Zwraca listę stringów
-            Assert.Equal(fileReader.GetRecords().First(), s);
-        }
-
-        [Fact]
-        public void testTest()
+        public void CheckIfFileReaderReturnsCorrectString()
         {
             var fileReader = Substitute.For<IGeoFileReader>();
 
             var repository = Substitute.For<IGeoRepository>();
 
-            var import = new GeoInport(fileReader, repository);
+            var import = new GeoImport(fileReader, repository);
 
-            string s = @"1.1-800/128
-        5544943.71        6532099.62         5544943.71        6532099.62 N  7
-7
-              1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7
-              1-7880        5544908.34        6532098.97        5544908.34        6532098.97   0 N N  7
-              1-7807        5544907.94        6532075.96        5544907.94        6532075.96   0 N N  7
-              1-7856        5544928.59        6532085.45        5544928.59        6532085.45   0 N N  7
-              1-7881        5544975.70        6532098.40        5544975.70        6532098.40   0 N N  7
-              1-7907        5544972.66        6532107.54        5544972.66        6532107.54   0 N N  7
-              1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7";
+            var defaultTown = import.Parse();
 
-            fileReader.GetRecords().Returns(new List<string> { s }); //Zwraca listę stringów
-
-            var tabStrings = s.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
-
-            string nrDzialki = tabStrings[0];
-
-            //rekordy
-            string x = tabStrings[1];
-            string y = tabStrings[2];
+            fileReader.GetRecords().Returns(new List<string> { defaultTown.defaultTown });
+            Assert.Equal(fileReader.GetRecords().First(), defaultTown.defaultTown);
         }
 
         [Fact]
-        public void CheckIfGeoNumer()
+        public void CheckIfParserReturnsCorrectValues()
         {
             var fileReader = Substitute.For<IGeoFileReader>();
 
             var repository = Substitute.For<IGeoRepository>();
 
-            var import = new GeoInport(fileReader, repository);
+            var import = new GeoImport(fileReader, repository);
 
-            string s = @"1.1-800/128
-        5544943.71        6532099.62         5544943.71        6532099.62 N  7
-7
-              1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7
-              1-7880        5544908.34        6532098.97        5544908.34        6532098.97   0 N N  7
-              1-7807        5544907.94        6532075.96        5544907.94        6532075.96   0 N N  7
-              1-7856        5544928.59        6532085.45        5544928.59        6532085.45   0 N N  7
-              1-7881        5544975.70        6532098.40        5544975.70        6532098.40   0 N N  7
-              1-7907        5544972.66        6532107.54        5544972.66        6532107.54   0 N N  7
-              1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7";
+            var defaultTown = import.Parse();
 
-            fileReader.GetRecords().Returns(new List<string> { s }); //Zwraca listę stringów
-
-            var tabStrings = s.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
-
-            string nrDzialki = tabStrings[0];
-
-            Assert.Equal(nrDzialki, "1.1-800/128");
-        }
-
-
-        [Fact]
-        public void checkIfXYDouble()
-        {
-            var fileReader = Substitute.For<IGeoFileReader>();
-
-            string s = @"1.1-800/128
-        5544943.71        6532099.62         5544943.71        6532099.62 N  7
-7
-              1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7
-              1-7880        5544908.34        6532098.97        5544908.34        6532098.97   0 N N  7
-              1-7807        5544907.94        6532075.96        5544907.94        6532075.96   0 N N  7
-              1-7856        5544928.59        6532085.45        5544928.59        6532085.45   0 N N  7
-              1-7881        5544975.70        6532098.40        5544975.70        6532098.40   0 N N  7
-              1-7907        5544972.66        6532107.54        5544972.66        6532107.54   0 N N  7
-              1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7";
-
-            fileReader.GetRecords().Returns(new List<string> { s }); //Zwraca listę stringów
-
-            var tabStrings = s.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
-            string x = tabStrings[1];
-            string y = tabStrings[2];
-
-            Convert.ToDouble(x);
-            Convert.ToDouble(y);
+            Assert.Equal(defaultTown.GeoNumber, "1.1-800/128");
+            Assert.Equal(defaultTown.x, 5544943.71);
+            Assert.Equal(defaultTown.y, 6532099.62);
+            Assert.Equal(defaultTown.maxX, 5544975.7);
+            Assert.Equal(defaultTown.maxY, 6532117.26);
+            Assert.Equal(defaultTown.minX, 5544907.94);
+            Assert.Equal(defaultTown.minY, 6532075.96);
         }
     }
 }
