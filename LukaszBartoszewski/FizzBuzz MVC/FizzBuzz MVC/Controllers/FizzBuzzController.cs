8ebd485f69f08fb316aa14381e6869c8eb9c902d
LucasBartoszewski <khoga91@gmail.com>
2014-03-24 18:08:20 +0100

    FizzBuzz MVC

---
+++ LukaszBartoszewski/FizzBuzz MVC/FizzBuzz MVC/Controllers/FizzBuzzController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace FizzBuzz_MVC.Controllers
+{
+    public class FizzBuzzController : ApiController
+    {
+        //
+        // GET: /FizzBuzz/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ LukaszBartoszewski/FizzBuzz MVC/FizzBuzz MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FizzBuzz_MVC.MvcApplication" Language="C#" %>
