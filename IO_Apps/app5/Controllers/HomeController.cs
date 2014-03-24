9d7254c132ac4996df77075cd22bbfca1bf2d37e
damlox <lorekd07d@gmail.com>
2014-03-24 18:00:51 +0100

    Lab 5

---
+++ IO_Apps/app5/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace app5.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index(int id = 1)
+        {
+            ViewBag.test = id;
+            return View();          
+        }
+	}
+}
