8438af1823bbd8a76985583b2f5ec22f04a430cc
AndrewKrawiec <andrewkrawiec@wp.pl>
2014-04-07 17:50:24 +0200

    LSP

---
+++ AndrzejKrawiec/LSP/LSP/Prostokat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LSP
+{
+    class Prostokat
+    {
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int liczPole()
+        {
+            return X * Y;
+        }
+
+        public int liczObwod()
+        {
+            return 2 * X + 2 * Y;
+        }
+    }
+}
---
+++ AndrzejKrawiec/LSP/LSP/Test.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LSP
+{
+    public class Test
+    {
+        [Fact]
+        public void Czy_obiekt_jest_kwadratem()
+        {
+            Kwadrat kwadrat = new Kwadrat();
+            kwadrat.X = 4;
+            kwadrat.Y = 4;
+
+            Assert.Equal(kwadrat.X, kwadrat.Y);
+        }
+
+        [Fact]
+        public void Czy_obiekt_jest_prostokatem()
+        {
+            Prostokat prostokat = new Prostokat();
+            prostokat.X = 4;
+            prostokat.Y = 6;
+
+            Assert.NotEqual(prostokat.X, prostokat.Y);
+        }
+
+        [Fact]
+        public void Pole_Prostoka()
+        {
+            Prostokat pro = new Prostokat();
+            pro.X = 4;
+            pro.Y = 6;
+            Assert.Equal(pro.liczPole(), 24);
+        }
+
+        [Fact]
+        public void Obwod_Prostokata()
+        {201
+            Prostokat pro = new Prostokat();
+            pro.X = 4;
+            pro.Y = 6;
+            Assert.Equal(pro.liczObwod(), 20);
+        }
+
+        [Fact]
+        public void Pole_Kwadrata()
+        {
+            Kwadrat pro = new Kwadrat();
+            pro.X = 4;
+            pro.Y = 4;
+            Assert.Equal(pro.liczPole(), 16);
+        }
+
+        [Fact]
+        public void Obwod_Kwadrata()
+        {
+            Kwadrat pro = new Kwadrat();
+            pro.X = 4;
+            pro.Y = 4;
+            Assert.Equal(pro.liczObwod(), 16);
+        }
+    }
+}
