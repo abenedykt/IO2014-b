1117ff33275daa8ce8d8c028844383598697547a
sd <pracowiciebezczynny@gmail.com>
2014-05-05 18:14:11 +0200

    Ndepend
    Ndepend

---
+++ Tomasz Borys/Ndepend1/ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApplication1
+{
+    class Program : CL1
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
