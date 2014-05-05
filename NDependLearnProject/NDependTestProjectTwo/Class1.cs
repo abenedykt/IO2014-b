d02f7e74db1812951d2998e83138297c6e62f52a
GrzegorzJanik <grzegorz0011@gmail.com>
2014-05-05 18:00:40 +0200

    Zajęcia 10.

---
+++ NDependLearnProject/NDependTestProjectTwo/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDependTestProjectTwo
+{
+    public abstract class Class1 : NDependTestProjectThree.Interface1
+    {
+
+    }
+}
