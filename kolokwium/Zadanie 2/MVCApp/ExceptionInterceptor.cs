0dbc550115fead6a50eb8869f595682b807f358a
Adul0 <adul01@gmail.com>
2014-05-26 17:26:58 +0200

    Ukonczenie interceptora

--- kolokwium/Zadanie 2/MVCApp/ExceptionInterceptor.cs
+++ kolokwium/Zadanie 2/MVCApp/ExceptionInterceptor.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Web;
 using Autofac.Extras.DynamicProxy2;
 using Castle.DynamicProxy;
+using System.Diagnostics;
 
 namespace MVCApp
 {
     public class ExceptionInterceptor : IInterceptor
     {
 
+        public void Intercept(IInvocation invocation)
+        {
+            try
+            {
+                invocation.Proceed();
+            }
+            catch(Exception e)
+            {
+                Trace.WriteLine(e.Message);
+            }
 
+        }
 
     }
 }
