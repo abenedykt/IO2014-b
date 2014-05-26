2f177decf067abb2ac747b44cda1915ffccd8c4e
pprzemyk <przemykpawel@gmail.com>
2014-05-26 17:05:32 +0200

    Kolokwium 1 część - TDD

---
+++ Kolokwium/FizzBuzz/FizzBuzz/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Game
+    {
+        public object Say(int p)
+        {
+            if (p == int.MaxValue)
+                return "MaxInt";
+            if (p == int.MinValue)
+                return "MinInt";
+            if(p<1)
+                return "Lower then one";
+            if (p%15 == 0)
+                return "FizzBuzz";
+            if (p%5 == 0)
+                return "Buzz";
+            return "Fizz";
+        }
+    }
+}
---
+++ Kolokwium/FizzBuzz/Tests/Test.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzz;
+using Xunit;
+
+namespace Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void For3SayFizz()
+        {
+            var game = new Game();
+            var r = game.Say(3);
+            Assert.Equal("Fizz", r);
+        }
+
+        [Fact]
+        public void For5SayBuzz()
+        {
+            var game = new Game();
+            var r = game.Say(5);
+            Assert.Equal("Buzz", r);
+        }
+
+        [Fact]
+        public void For15SayFizzBuzz()
+        {
+            var game = new Game();
+            var r = game.Say(15);
+            Assert.Equal("FizzBuzz", r);
+        }
+
+        [Fact]
+        public void ForMaxIntSayMaxInt()
+        {
+            var game = new Game();
+            var r = game.Say(int.MaxValue);
+            Assert.Equal("MaxInt", r);
+        }
+
+        [Fact]
+        public void ForMinIntSayMinInt()
+        {
+            var game = new Game();
+            var r = game.Say(int.MinValue);
+            Assert.Equal("MinInt", r);
+        }
+
+        [Fact]
+        public void For0SayLowerThenOne() // liczymy od 1 w gore
+        {
+            var game = new Game();
+            var r = game.Say(0);
+            Assert.Equal("Lower then one", r);
+        }
+
+        [Fact]
+        public void For9IntSayFizz()
+        {
+            var game = new Game();
+            var r = game.Say(9);
+            Assert.Equal("Fizz", r);
+        }
+
+        [Fact]
+        public void For55IntSayBuzz()
+        {
+            var game = new Game();
+            var r = game.Say(55);
+            Assert.Equal("Buzz", r);
+        }
+
+        [Fact]
+        public void For45SayFizzBuzz()
+        {
+            var game = new Game();
+            var r = game.Say(45);
+            Assert.Equal("FizzBuzz", r);
+        }
+
+        [Fact]
+        public void ForMinus5SayLowerThenOne() // liczymy od 1 w gore
+        {
+            var game = new Game();
+            var r = game.Say(-5);
+            Assert.Equal("Lower then one", r);
+        }
+    }
+}
