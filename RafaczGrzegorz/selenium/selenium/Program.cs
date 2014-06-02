7087f8a4a65495e47306923e4a5a1e1809d64109
GrzegorzRafacz <grzechuu42@o2.pl>
2014-06-02 18:00:46 +0200

    testy:D

---
+++ RafaczGrzegorz/selenium/selenium/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Extensions;
+using Xunit;
+
+namespace selenium
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+    {
+     
+                
+            
+        }
+
+       [Fact]
+       public void testowy()
+       {
+           var driver = new ChromeDriver();
+
+           driver.Navigate().GoToUrl("http://webcalculator.apphb.com");
+           driver.FindElement(By.Name("A")).SendKeys("3");
+           driver.FindElement(By.Name("B")).SendKeys("5");
+           driver.FindElement(By.Name("Add")).Click();
+           string test = driver.FindElement(By.TagName("h4")).Text;
+           driver.Close();
+           Assert.Equal("15", test);
+
+       }
+
+    }
+}
